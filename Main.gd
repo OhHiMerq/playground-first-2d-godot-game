9a5a234d66b9486fe93e86994054848470d821d6
OhHiMerq <dimailigv@gmail.com>
2023-02-04 19:12:22 +0800

    connect hud to main scene

--- Main.gd
+++ Main.gd
@@ -5,22 +5,26 @@ var score
 
 func _ready():
 	randomize()
-	new_game()
 
 
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_StartTimer_timeout():
@@ -52,3 +56,4 @@ func _on_MobTimer_timeout():
 	
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
+
